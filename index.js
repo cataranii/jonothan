e7105e325a6372f9579df735c58d854c79aeb6ee
cataranii <qwershiba@outlook.com>
2026-02-27 16:53:17 +0000

    update

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 console.log("fart");
 
-window.alert("HELLO");
+console.log(`Dies iræ, dies illa
+Solvet sæclum in favilla
+Teste David cum Sibylla
+Quantus tremor est futurus
+Quando iudex est venturus
+Cuncta stricte discussurus
+Dies iræ, dies illa
+Solvet sæclum in favilla
+Teste David cum Sibylla
+Quantus tremor est futurus
+Quando iudex est venturus
+Cuncta stricte discussurus
+Quantus tremor est futurus
+Dies iræ, dies illa
+Quantus tremor est futurus
+Dies iræ, dies illa
+Quantus tremor est futurus
+Quantus tremor est futurus
+Quando iudex est venturus
+Cuncta stricte discussurus
+Cuncta stricte (cuncta stricte)
+Stricte discussurus
+Cuncta stricte (cuncta stricte)
+Stricte discussurus`);
+
+window.alert(`MADE BY MR HERRON`);
